bb903bc374b98e6a7eb90ca3d1220ceb33dea458
coldbell8918 <pjc0525@hanyang.ac.kr>
2024-08-01 16:07:29 +0900

    bookstore

--- launch/hospital.launch.py
+++ launch/hospital.launch.py
@@ -6,22 +6,37 @@ from launch.conditions import IfCondition
 from launch.substitutions import PythonExpression
 from ament_index_python.packages import get_package_share_directory
 
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
 
 def generate_launch_description():
     world_file_name = "hospital.world"
     world = os.path.join(get_package_share_directory('aws_robomaker_hospital_world'), 'worlds', world_file_name)
 
+    launch_file_dir = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch')
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+
     gazebo_ros = get_package_share_directory('gazebo_ros')
     gazebo_client = launch.actions.IncludeLaunchDescription(
-	launch.launch_description_sources.PythonLaunchDescriptionSource(
+	    PythonLaunchDescriptionSource(
             os.path.join(gazebo_ros, 'launch', 'gzclient.launch.py')),
         condition=launch.conditions.IfCondition(launch.substitutions.LaunchConfiguration('gui'))
      )
     gazebo_server = launch.actions.IncludeLaunchDescription(
-        launch.launch_description_sources.PythonLaunchDescriptionSource(
+        PythonLaunchDescriptionSource(
             os.path.join(gazebo_ros, 'launch', 'gzserver.launch.py'))
     )
 
+    robot_state_publisher_cmd = launch.actions.IncludeLaunchDescription(
+
+        PythonLaunchDescriptionSource(
+            os.path.join(launch_file_dir, 'robot_state_publisher.launch.py')
+        ),
+        launch_arguments={'use_sim_time': use_sim_time}.items()
+    )
 
     ld = launch.LaunchDescription([
         launch.actions.DeclareLaunchArgument(
@@ -33,7 +48,8 @@ def generate_launch_description():
             default_value='false'
         ),
         gazebo_server,
-        gazebo_client
+        gazebo_client,
+        robot_state_publisher_cmd
     ])
     return ld
 
